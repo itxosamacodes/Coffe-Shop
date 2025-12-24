5e89c1d0f3896a12f663445ee627a28db4035efd
itxosamacodes <itxosama.io@gmail.com>
2025-12-24 15:59:51 +0500

    mapBox update

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -2,9 +2,7 @@ import MapboxGL from "@rnmapbox/maps";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
-MapboxGL.setAccessToken(
-  "pk.eyJ1Ijoic2hvYWliMjciLCJhIjoiY21qaDBqOXhyMTZtODNjcXpwdzk1a3U5MyJ9.nX11n_uZd8pgvX9PC3IDgA"
-);
+MapboxGL.setAccessToken(process.env.MAPBOX_ACCESS_TOKEN!);
 
 export default function MapScreen() {
   return (
